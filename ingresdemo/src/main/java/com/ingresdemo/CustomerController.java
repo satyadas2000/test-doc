64cfda7e5c90688cbe699c975079ea4751ea0e35
satyadas2000 <satyadas20000@gmail.com>
2020-04-15 15:30:21 +0530

    ddddfd

---
+++ ingresdemo/src/main/java/com/ingresdemo/CustomerController.java
@@ -0,0 +1,21 @@
+package com.ingresdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+ 
+@RestController
+@RequestMapping("/ingresdemo")
+public class CustomerController
+{
+    @RequestMapping("/customers")
+    public List<Customer> findAll()
+    {
+      List<Customer> customerList = new ArrayList<Customer>();
+      customerList.add(new Customer(1, "satya"));
+      customerList.add(new Customer(2, "john"));
+      return customerList;
+    }
+}
